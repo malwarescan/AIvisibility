e80bcba59fcc853ca2912a1ecbf32f9e11909256
Joel Maldonado <domtheworldllc@gmail.com>
2025-10-11 20:16:00 +0700

    Make language switcher even smaller
    - Reduce button padding from px-1 py-0.5 to px-0.5 py-0
    - Reduce icon size from h-2.5 w-2.5 to h-2 w-2
    - Reduce dropdown width from w-32 to w-24
    - Reduce dropdown item padding from px-3 py-1 to px-2 py-0.5
    - More compact appearance in navigation

--- templates/language-switcher.php
+++ templates/language-switcher.php
@@ -7,7 +7,7 @@ $switcher = I18n::getLanguageSwitcher($_SERVER['REQUEST_URI']);
 
 <div class="language-switcher">
     <div class="relative inline-block text-left">
-        <button type="button" class="inline-flex items-center px-1 py-0.5 text-xs font-medium text-gray-600 hover:text-gray-900 focus:outline-none" id="language-menu-button" aria-expanded="false" aria-haspopup="true">
+        <button type="button" class="inline-flex items-center px-0.5 py-0 text-xs font-medium text-gray-600 hover:text-gray-900 focus:outline-none" id="language-menu-button" aria-expanded="false" aria-haspopup="true">
             <span class="current-lang">
                 <?php foreach ($switcher as $lang): ?>
                     <?php if ($lang['current']): ?>
@@ -15,16 +15,16 @@ $switcher = I18n::getLanguageSwitcher($_SERVER['REQUEST_URI']);
                     <?php endif; ?>
                 <?php endforeach; ?>
             </span>
-            <svg class="ml-0.5 h-2.5 w-2.5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <svg class="ml-0.5 h-2 w-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
             </svg>
         </button>
 
-        <div class="origin-top-right absolute right-0 mt-1 w-32 rounded shadow-lg bg-white border border-gray-200 focus:outline-none hidden" role="menu" aria-orientation="vertical" aria-labelledby="language-menu-button" tabindex="-1" id="language-menu">
+        <div class="origin-top-right absolute right-0 mt-1 w-24 rounded shadow-lg bg-white border border-gray-200 focus:outline-none hidden" role="menu" aria-orientation="vertical" aria-labelledby="language-menu-button" tabindex="-1" id="language-menu">
             <div class="py-1" role="none">
                 <?php foreach ($switcher as $lang): ?>
                     <a href="<?= htmlspecialchars($lang['url']) ?>" 
-                       class="<?= $lang['current'] ? 'bg-gray-100 text-gray-900' : 'text-gray-700' ?> block px-3 py-1 text-xs hover:bg-gray-100" 
+                       class="<?= $lang['current'] ? 'bg-gray-100 text-gray-900' : 'text-gray-700' ?> block px-2 py-0.5 text-xs hover:bg-gray-100" 
                        role="menuitem" 
                        tabindex="-1">
                         <?= htmlspecialchars($lang['name']) ?>
